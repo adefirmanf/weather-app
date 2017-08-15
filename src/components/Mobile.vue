60976c88b5c8ebc81e157bdff70bf98ffff3f89d
adefirmanf <syskeys32@gmail.com>
2017-08-15 20:28:36 +0700

    Adding Onsen.UI

---
+++ src/components/Mobile.vue
@@ -0,0 +1,25 @@
+
+  <template>
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">Title</div>
+      </v-ons-toolbar>
+      <p style="text-align: center">
+        <v-ons-button @click="$ons.notification.alert('Hello World!')">
+          Click me!
+        </v-ons-button>
+      </p>
+    </v-ons-page>
+  </template>
+<script>
+/* eslint semi: ["error", "never"] */
+  export default {
+    data() {
+      return {
+
+      }
+    },
+  }
+</script>
+</body>
+</html>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
+/* eslint-disable */
+import 'onsenui/css/onsenui.css';
+import 'onsenui/css/onsen-css-components.css';
+
 import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
+import Mobile from '@/components/Mobile';
+
+import VueOnsen from 'vue-onsenui'
+Vue.use(VueOnsen);
 
 Vue.use(Router);
-
 export default new Router({
   routes: [
     {
@@ -11,5 +18,11 @@ export default new Router({
       name: 'Hello',
       component: Hello,
     },
+    {
+      path : '/mobile',
+      name : 'Mobile',
+      component : Mobile
+	}
+    
   ],
 });
