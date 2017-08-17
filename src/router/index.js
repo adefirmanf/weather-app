d35c23ea8925c1e3e279dd65ffc3e3d3a172a3f7
adefirmanf <syskeys32@gmail.com>
2017-08-17 19:19:55 +0700

    Support webpack for cordova. Refer to my public gist

--- src/router/index.js
+++ src/router/index.js
@@ -6,23 +6,23 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Hello from '@/components/Hello';
 import Mobile from '@/components/Mobile';
-
 import VueOnsen from 'vue-onsenui'
-Vue.use(VueOnsen);
 
+Vue.use(VueOnsen);
 Vue.use(Router);
+
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello,
-    },
-    {
-      path : '/mobile',
-      name : 'Mobile',
-      component : Mobile
-	}
+      name: 'Mobile',
+      component: Mobile,
+    }
+ //    {
+ //      path : '/mobile',
+ //      name : 'Mobile',
+ //      component : Mobile
+	// }
     
   ],
 });
